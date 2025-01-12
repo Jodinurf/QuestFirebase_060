2e031a281d0b56ab3c2b9400037787f401f96962
KhodamIblis <jodinurfarkhani04@gmail.com>
2025-01-13 00:59:22 +0700

    perbaikan pada operasi delete dengan menghapus berdasarkan field (nim) karena membaca ID berdasarkan NIM, ID disimpan dengan auto ID bukan nim

--- app/src/main/java/com/jodifrkh/firebase/repository/NetworkMahasiswaRepository.kt
+++ app/src/main/java/com/jodifrkh/firebase/repository/NetworkMahasiswaRepository.kt
@@ -1,6 +1,5 @@
 package com.jodifrkh.firebase.repository
 
-import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import com.jodifrkh.firebase.model.Mahasiswa
@@ -31,7 +30,16 @@ class NetworkMahasiswaRepository (
 
     override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
         try {
-            firestore.collection("Mahasiswa").add(mahasiswa).await()
+            // jika ingin menyimpan dokumen ID berdasarkan NIM, gunakan komentar
+//            val nim = mahasiswa.nim
+//            if (nim.isNotEmpty()) {
+//                throw Exception("NIM Tidak boleh kosong")
+//            }
+            firestore.collection("Mahasiswa")
+                //.document(nim)
+                //set(mahasiswa) jangan gunakan add(mahasiswa) karena akan otomatis Auto ID Documents
+                .add(mahasiswa)
+                .await()
         } catch (e: Exception) {
             throw Exception("Gagal menambahkan data Mahasiswa: ${e.message}")
         }
@@ -48,16 +56,36 @@ class NetworkMahasiswaRepository (
         }
     }
 
+    // gunakan fungsi dibawah untuk proses penghapusan data berdasarkan field, karena dokumen ID auto ID, bukan NIM
     override suspend fun deleteMahasiswa(mahasiswa: Mahasiswa) {
         try {
-            firestore.collection("Mahasiswa")
-                .document(mahasiswa.nim)
-                .delete()
-                .await()
+            val querySnapshot = // membuat variabel terlebih dahulu
+                firestore.collection("Mahasiswa")
+                    .whereEqualTo("nim", mahasiswa.nim) // menggunakan fungsi whereEqualto untuk membuat query yang mencari fields nim dalam koleksi Mahasiswa
+                    .get() // mengambil data
+                    .await()
+
+            for (document in querySnapshot.documents) { // melakukan perulangan dokumen selama variabel mencari nim
+                document.reference // menunjuk dokumen yang terkait berdasarkan perulangan
+                    .delete() // melakukan penghapusan dokumen
+                    .await()
+            }
         } catch (e: Exception) {
             throw Exception("Gagal Menghapus data Mahasiswa: ${e.message}")
         }
     }
+
+    // Gunakan fungsi di bawah apabila dokumen ID adalah nim yang akan dihapus, bukan auto ID
+//    override suspend fun deleteMahasiswa(mahasiswa: Mahasiswa) {
+//        try {
+//            firestore.collection("Mahasiswa")
+//                .document(mahasiswa.nim)
+//                .delete()
+//                .await()
+//        } catch (e: Exception) {
+//            throw Exception("Gagal Menghapus data Mahasiswa: ${e.message}")
+//        }
+//    }
 
     override suspend fun getMahasiswaByNim(nim: String): Flow<Mahasiswa> = callbackFlow {
         val mhsDocument = firestore.collection("Mahasiswa")
